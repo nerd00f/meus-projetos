3069e071a0304e3d4ad7776131b9d0efd4fae111
nerd00f <alancsystem@gmail.com>
2022-02-17 18:05:02 -0300

    remover espacos

---
+++ Estudos em Livros/Curso intensivo de python/05.Remover_espacos/main.py
@@ -0,0 +1,10 @@
+# declara variavel
+var = '   variavel    '
+
+# removes espacos direita, esquerda
+# mostra variavel na tela
+print(var.strip())
+
+# .rstrip() remove a direita
+# .lstrip() remove a esquerda
+
