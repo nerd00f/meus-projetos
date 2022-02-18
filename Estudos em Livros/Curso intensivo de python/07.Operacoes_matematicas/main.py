d6d041a94a7d29c1f53c9497c075a81131282926
Alan Christian <alancsystem@gmail.com>
2022-02-18 00:56:56 +0000

    mexendo com numeros e arruma codigo

---
+++ Estudos em Livros/Curso intensivo de python/07.Operacoes_matematicas/main.py
@@ -0,0 +1,10 @@
+num1 = 5
+num2 = 3
+
+print('O primeiro número é:', num1)
+print('O segundo número é:', num2)
+print('- -')
+print('E sua adição corresponde a:', num1 + num2)
+print('E sua subtração corresponde a:', num1 - num2)
+print('E sua multiplicação corresponde a:', num1 * num2)
+print('E sua divisão corresponde a:', num1 / num2)
--- Outros Estudos/Estudos em Python/0. Basico/1.Soma_numerica.py
+++
@@ -1,6 +0,0 @@
-# Crie um script python que leia dois numeros
-#  e mostre a soma entre eles
-
-num1 = int(input('Primeiro número '))
-num2 = int(input('Segundo número '))
-print('A soma é', num1 + num2)
