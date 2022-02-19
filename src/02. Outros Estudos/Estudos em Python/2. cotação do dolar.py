1af916bc4160b5aa1ff934622abe9cb68d6857dd
Alan Christian <alancsystem@gmail.com>
2022-02-19 20:31:44 +0000

    commit

--- src/02. Outros Estudos/Estudos em Python/2. cotação do dolar.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-requisicao = requests.get('https://economia.awesomeapi.com.br/all/USD-BRL')
-
-cotacao = json.loads(requisicao.text)
-
-print("valor do dolar:", cotacao['USD']['ask'])
--- src/02. Outros Estudos/Estudos em Python/3. consulta do clima.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-requisicao = requests.get('http://apiadvisor.climatempo.com.br/api/v1/anl/synoptic/locale/BR?token=56eda943f4060cc966dac392cf48d1e8')
-
-clima = json.loads(requisicao.text)
-
-print(clima[0]['text'])
